cb7ee723df8859a3b94696b3634a8f9823e08a50
Luca-Mariselli <lucamariselli@gmail.com>
2024-06-10 15:17:57 +0200

    first commit

---
+++ data.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php 
+$nome = $_GET['nome'] ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nome:</h1>
+    <ul>
+        <li><?php echo $nome?></li>
+    </ul>
+</body>
+</html>
